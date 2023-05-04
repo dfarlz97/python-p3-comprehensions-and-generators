73b1a7a4fcd7bac7ea9e69e02139baaf758890ad
+ <+>
2023-05-04 16:11:19 -0400

    write list comp funcs

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
-
+    num_list = [(n) for n in num_list if (n % 2 == 0)]
+    return num_list
 def make_exclamation(sentence_list):
-    pass
+    sentence_list = [(sent + "!") for sent in sentence_list]
+    return sentence_list
